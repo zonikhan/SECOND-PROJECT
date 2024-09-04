eea7a084b939fec0308710aa0cce1bdf51dd3e4a
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-09-04 15:45:51 +0500

    second project

---
+++ page.tsx
@@ -0,0 +1,13 @@
+export default function home(){
+  return (
+    <div>
+      <h1> Rabia </h1>
+        <h2> passionate frontend webdeveloper</h2>
+        <h3>Experienced social media marketer</h3>
+        <h4> Skilled freelance  professional</h4>
+        <h5>Createtive  content writer</h5>
+        <h6> Versatile Digital Marketing Specialist</h6>
+    </div>
+  );
+}
+
